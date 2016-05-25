0ba3e35c4202eeadbcb455f913b46fc2f9eefbca
iliyanvaklinov <i_vack@mail.bg>
2016-05-25 20:11:13 +0300

    Модул 02, Видео 01-Добяване на коментари

---
+++ Modul02/Modul02/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Modul02
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+
+		//дефиниране на числа
+
+		//събиране на числа
+
+		//писане в конзола
+			   
+		}
+	}
+}
