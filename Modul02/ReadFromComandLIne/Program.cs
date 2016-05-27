2e6726d96e7bf2f2fd0cc5eed1b3490816437af5
iliyanvaklinov <i_vack@mail.bg>
2016-05-27 21:55:54 +0300

    Модул 02, Видео 06- Работа с стрингове

--- Modul02/ReadFromComandLIne/Program.cs
+++ Modul02/ReadFromComandLIne/Program.cs
@@ -9,12 +9,27 @@ namespace ReadFromComandLIne
 			//дефиниране на променливи
 
 			int a = 5, b = 0;
+
 			//Въвежда не входни променливи
+
 			Console.Write("а=5"+"\n");
 			Console.Write("Моля въведете b: " );
 			b=Convert.ToInt32(Console.ReadLine() ); 
-			//Пе1ат на резултат
+
+			//Печат на резултат
+
 			Console.WriteLine ("Резултата a + b = "+(a + b).ToString ()+"\n\n\n");
+
+			string test = "1";
+			test += ", 2";
+			test += ", 99";
+			test += ", 4";
+			test += ", 5";
+			Console.WriteLine ("Събиране с +=  : "+test+ "\n" );
+			test=test.Replace (", ", ";");
+			Console.WriteLine ("Работа с Replace  "+test+ "\n" );
+			Console.WriteLine ("3 ти знак  "+test.Split (';') [2] );
+
 		}
 	}
 }
